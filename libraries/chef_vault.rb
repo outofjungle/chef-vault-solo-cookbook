8ab2c3c3063dc3d1d259eb1ce7dc5fe523d4ff0a
Venkat Venkataraju <venkat.venkataraju@yahoo.com>
2013-12-11 10:50:00 -0800

    making chef-vault work for second time

--- libraries/chef_vault.rb
+++ libraries/chef_vault.rb
@@ -1,11 +1,15 @@
-class ChefVault::Item
-  self.singleton_class.send(:alias_method, :load_vault, :load)
+require 'chef-vault'
 
-  def self.load(vault, name)
-    if (Chef::Config[:solo])
-      Chef::DataBagItem.load(vault, name)
-    else
-      ChefVault::Item.load_vault(vault, name)
+class ChefVault
+  class Item < Chef::DataBagItem
+    self.singleton_class.send(:alias_method, :load_vault, :load)
+
+    def self.load(vault, name)
+      if (Chef::Config[:solo])
+        Chef::DataBagItem.load(vault, name)
+      else
+        ChefVault::Item.load_vault(vault, name)
+      end
     end
   end
 end
